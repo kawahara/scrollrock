5e2cbf9f9f2a210611bb3aaed292a8b44eafa192
Shogo Kawahara <kawahara@bucyou.net>
2012-03-10 01:35:31 +0900

    Change the way to send update message for parent screen

--- public/javascripts/scrollrock.js
+++ public/javascripts/scrollrock.js
@@ -4,13 +4,16 @@ var next;
 var previous;
 var gotoSection;
 $(function() {
-  var WS = window['MozWebSocket'] ? MozWebSocket : WebSocket;
-  if (WS != null) {
+  var WS = window['MozWebSocket'] ? MozWebSocket : WebSocket
+    , lastScrollTop = $(document).scrollTop()
+    , isUpdate = false;
+
+  if (WS !== null) {
     conn = new WS('ws://' + document.location.host + "/", 'scrollrock');
     conn.onmessage = function(event) {
-      if (event.data == 'next') {
+      if (event.data === 'next') {
         next();
-      } else if (event.data == 'previous') {
+      } else if (event.data === 'previous') {
         previous();
       }
     };
@@ -24,63 +27,76 @@ $(function() {
   };
 
   gotoSection = function(sectionNumber) {
-    var obj = $('section').get(sectionNumber - 1);
+    var obj = $('section').get(sectionNumber - 1),
+        t;
     if (obj !== null) {
-      var t = $(obj).position().top;
-      $('html,body').animate({ scrollTop: t }, 'slow');
+      t = $(obj).position().top;
+      $('html,body').animate({ scrollTop: t }, 'slow', null, function() {
+      });
     }
-    updateCurrentPage();
   };
 
   next = function(event) {
-    var st = $(document).scrollTop();
+    var st = $(document).scrollTop(), t;
     $('section').each(function () {
-      var t = $(this).position().top;
+      t = $(this).position().top;
       if (st < t) {
-        $('html,body').animate({ scrollTop: t }, 'slow');
+        $('html,body').animate({ scrollTop: t }, 'slow', null, function() {
+        });
         return false;
       }
     });
-    updateCurrentPage();
   };
 
   previous = function(event) {
-    var st = $(document).scrollTop();
+    var st = $(document).scrollTop(), t;
     $($('section').get().reverse()).each(function() {
-      var t = $(this).position().top;
+      t = $(this).position().top;
       if (st > t) {
-        $('html,body').animate({ scrollTop: t }, 'slow');
+        $('html,body').animate({ scrollTop: t }, 'slow', null, function() {
+        });
         return false;
       }
     });
-    updateCurrentPage();
   };
 
-  var updateCurrentPage = function(event) {
-    var st = $(document).scrollTop();
-    var page = 0;
+  var updateCurrentPage = function() {
+    var st = $(document).scrollTop()
+      , page = 0
+      , t;
     $('section').each(function () {
-      var t = $(this).position().top;
+      t = $(this).position().top;
       if (st < t) {
         return false;
       }
-      page++;
+      page+=1;
     });
 
     if (conn.readyState === 1) {
-        conn.send(JSON.stringify({'update': {'top': 0, 'page': page}}));
+      conn.send(JSON.stringify({'update': {'top': st, 'page': page}}));
     }
 
     currentPage = page;
   };
 
   $(window).resize(ajust);
-  $(window).scroll(updateCurrentPage);
   $(window).keypress(function(event) {
     switch(event.keyCode) {
       case 37: previous(); break;
       case 39: next(); break;
     }
   });
+  var observeScrollTop = function() {
+    var st = $(document).scrollTop();
+    if (lastScrollTop !== st) {
+      isUpdate = true;
+    } else if (isUpdate) {
+      isUpdate = false;
+      updateCurrentPage();
+    }
+    lastScrollTop = st;
+    setTimeout(observeScrollTop, 300);
+  };
+  setTimeout(observeScrollTop, 300);
   ajust();
 });
