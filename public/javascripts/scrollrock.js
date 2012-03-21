06d1444df75035d0f207de20462d3a05eb032aab
Shogo Kawahara <kawahara@bucyou.net>
2012-03-21 23:09:41 +0900

    Refactor frontend javascript

--- public/javascripts/scrollrock.js
+++ public/javascripts/scrollrock.js
@@ -1,15 +1,109 @@
-var conn;
-var currentPage;
-var next;
-var previous;
-var gotoSection;
-$(function() {
-  var WS = window['MozWebSocket'] ? MozWebSocket : WebSocket
-    , lastScrollTop = $(document).scrollTop()
-    , isUpdate = false
-    , token1 = null
-    , token2 = null;
+var ScrollRock = function() {
+  this.currentPage = 1;
+  this.observeScInterval = 500;
+  this.timer = null;
+  this.conn = null;
+  this.isUpdate = false;
+  this.lastScrollTop = $(document).scrollTop();
+};
+ScrollRock.prototype.init = function() {
+  var t = this;
+  this.startObserveScrollTop();
+  this.adjustSectionHeight();
+  $(window).resize(function() {
+    t.adjustSectionHeight.apply(t);
+  });
+};
+ScrollRock.prototype.stopObserveScrollTop = function() {
+  if (this.timer !== null)
+    clearTimeout(this.timer);
+};
+ScrollRock.prototype.startObserveScrollTop = function(force) {
+  var t = this;
+  force = force || false;
+  if (this.timer === null || force)
+    this.timer = setTimeout(function () {
+      t.observeScrollTop.apply(t);
+    }, this.observeScInterval);
+};
+ScrollRock.prototype.observeScrollTop = function(event) {
+  var st = $(document).scrollTop();
+  if (this.lastScrollTop !== st) {
+    this.isUpdate = true;
+  } else if (this.isUpdate) {
+    this.isUpdate = false;
+    this.updateCurrentPage();
+  }
+  this.lastScrollTop = st;
+  this.startObserveScrollTop(true);
+};
+ScrollRock.prototype.gotoSection = function(page) {
+  var sections = $('section')
+    , obj
+    , topp
+    , t = this;
 
+  if (page < 1) {
+    page = 1;
+  } else if (page > sections.length) {
+    page = sections.length;
+  }
+  obj = sections.get(page - 1);
+
+  if (obj !== null) {
+    topp = $(obj).position().top;
+    $('html,body').animate({ scrollTop: topp }, 'slow', null, function() {
+        t.updateCurrentPage();
+    });
+  }
+};
+ScrollRock.prototype.next = function () {
+  console.log("Count Down: " + (this.currentPage - 1));
+  this.gotoSection(this.currentPage - 1);
+};
+ScrollRock.prototype.previous = function() {
+  console.log("Count Up: " + (this.currentPage + 1));
+  this.gotoSection(this.currentPage + 1);
+};
+ScrollRock.prototype.updateCurrentPage = function() {
+  var st = $(document).scrollTop()
+    , page = 0
+    , t;
+
+  $('section').each(function () {
+    t = $(this).position().top;
+    if (st < t) {
+      return false;
+    }
+    page+=1;
+  });
+
+  if (this.conn && this.conn.readyState === 1) {
+    this.conn.send(JSON.stringify({'update': {'top': st, 'page': page}}));
+  }
+
+  this.currentPage = page;
+};
+ScrollRock.prototype.adjustSectionHeight = function() {
+  var page = this.currentPage;
+  $('section').css('height', $(window).innerHeight());
+  this.gotoSection(page);
+};
+var scrollrock = new ScrollRock();
+
+$(function() {
+  var conn
+    , WS = window['MozWebSocket'] ? MozWebSocket : WebSocket
+    , token1 = null
+    , token2 = null
+    , controllerPanel = $('<div>');
+
+  // init scrollrock screen
+  scrollrock.init();
+
+  //TODO: init controller panel
+
+  // init WebSocket connection
   if (WS !== null) {
     conn = new WS('ws://' + document.location.host + "/", 'scrollrock');
     conn.onmessage = function(event) {
@@ -20,13 +114,13 @@ $(function() {
         token2 = data.token2;
       } else if (data.operate === 'goto' && data.params.page) {
         if (data.params.page === '+') {
-          next();
+          scrollrock.next();
         } else if (data.params.page === '-') {
-          previous();
+          scrollrock.previous();
         } else {
           var page = parseInt(data.params.page);
           if (!isNaN(page) && page > 0) {
-            gotoSection(page);
+            scrollrock.gotoSection(page);
           }
         }
       }
@@ -34,83 +128,15 @@ $(function() {
     conn.onopen = function(event) {
       conn.send(JSON.stringify({'parent': {}}));
     };
+
+    scrollrock.conn = conn;
   }
 
-  var ajust = function(event) {
-    $('section').css('height', $(window).innerHeight());
-  };
-
-  gotoSection = function(sectionNumber) {
-    var obj = $('section').get(sectionNumber - 1),
-        t;
-    if (obj !== null) {
-      t = $(obj).position().top;
-      $('html,body').animate({ scrollTop: t }, 'slow', null, function() {
-      });
-    }
-  };
-
-  next = function(event) {
-    var st = $(document).scrollTop(), t;
-    $('section').each(function () {
-      t = $(this).position().top;
-      if (st < t) {
-        $('html,body').animate({ scrollTop: t }, 'slow', null, function() {
-        });
-        return false;
-      }
-    });
-  };
-
-  previous = function(event) {
-    var st = $(document).scrollTop(), t;
-    $($('section').get().reverse()).each(function() {
-      t = $(this).position().top;
-      if (st > t) {
-        $('html,body').animate({ scrollTop: t }, 'slow', null, function() {
-        });
-        return false;
-      }
-    });
-  };
-
-  var updateCurrentPage = function() {
-    var st = $(document).scrollTop()
-      , page = 0
-      , t;
-    $('section').each(function () {
-      t = $(this).position().top;
-      if (st < t) {
-        return false;
-      }
-      page+=1;
-    });
-
-    if (conn.readyState === 1) {
-      conn.send(JSON.stringify({'update': {'top': st, 'page': page}}));
-    }
-
-    currentPage = page;
-  };
-
-  $(window).resize(ajust);
+  // key bind
   $(window).keypress(function(event) {
     switch(event.keyCode) {
-      case 37: previous(); break;
-      case 39: next(); break;
+      case 39: scrollrock.previous(); break;
+      case 37: scrollrock.next(); break;
     }
   });
-  var observeScrollTop = function() {
-    var st = $(document).scrollTop();
-    if (lastScrollTop !== st) {
-      isUpdate = true;
-    } else if (isUpdate) {
-      isUpdate = false;
-      updateCurrentPage();
-    }
-    lastScrollTop = st;
-    setTimeout(observeScrollTop, 300);
-  };
-  setTimeout(observeScrollTop, 300);
-  ajust();
 });
